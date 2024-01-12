842df67d301d0cc098e2395ad64396bb225db4bc
admin <vlad-kempo@yandex.ru>
2024-01-12 20:49:09 +0500

    added help item menu

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +28,39 @@ class MainActivity : AppCompatActivity() {
         val toolBar = binding.toolBar
         setSupportActionBar(toolBar)
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        toolBar.setupWithNavController(navController, appBarConfiguration)
+
+
 
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
+    }
+
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        Toast.makeText(this, "Айтем селектед запустился", Toast.LENGTH_SHORT).show()
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        Toast.makeText(this, "Nav controller started", Toast.LENGTH_SHORT).show()
+        return item.onNavDestinationSelected(navController)
+                || super.onOptionsItemSelected(item)
+
+        Log.d("MainActivity", item.toString())
+
+
     }
 }
